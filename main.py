b9e2e92397ebaab42d1789cadee2c74fcd9df380
Harshal Paliwal <paliwalharshal03@gmail.com>
2026-04-01 22:03:38 +0530

    Pydantic and Path and Query Parameters

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI 
+from pydantic import BaseModel
 
 app= FastAPI()
+
+class Developer(BaseModel):
+    username:str
+    email:str
+    github_handle:str
+    
 
 @app.get('/')
 def home():
@@ -10,7 +17,19 @@ def home():
 def health_check():
     return {'status':"ok"}
 
+@app.get("/developer")
+def list_developer(limit: int = 10, active: bool = True):
+    return {"limit": limit, "active": active}
+
+@app.get("/commits")
+def get_commits(username:str, limit:int=10, page:int=1):
+    skip = (page-1)*limit
+    return {"username":username, "limit":limit, "page":page, "skip":skip}    
+
 @app.get('/developer/{username}')
 def get_developer(username:str):
-    return {'username':username}
+    return {'username':username} 
 
+@app.post('/developer')
+def create_developer(dev: Developer):
+    return {"message": f"developer {dev.username} registered successfully", "data": dev}
