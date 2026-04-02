e1999a2cfe1cb665a8af5ce29a8a9a272d0e072a
Harshal Paliwal <paliwalharshal03@gmail.com>
2026-04-02 14:37:56 +0530

    Day 3: Database setup, SQLAlchemy models, and Alembic migrations

---
+++ models.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from database import Base
+
+class Developer(Base):
+    __tablename__= "developers"
+
+    id= Column(Integer, primary_key= True, index =True)
+    username = Column(String, unique= True, index =True)
+    email = Column(String, unique= True, index =True)
+    github_handle = Column(String)
+    is_active = Column(Boolean, default=True)
+
+class CommitLog(Base):
+    __tablename__= "commit_logs"
+
+    id = Column(Integer, primary_key=True, index=True)
+    message = Column(String)
+    developer_id = Column(Integer, ForeignKey("developers.id"))
+    
+    
